380d5cd1dcb5c1a85bfce0f4015835b8ac909051
Olivia Huang <oliviahyacinth@gmail.com>
2020-01-19 22:29:33 -0500

    Add controller and model to return datetime in /api/datetime endpoint, also disable https redirect for now

--- src/NowWebApp/Startup.cs
+++ src/NowWebApp/Startup.cs
@@ -41,7 +41,7 @@ namespace NowWebApp
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
